0c8dbea0831cded4f677145dc8fdde0f0f0298df
Siddhardha <siddhardharamannem@gmail.com>
2026-06-28 14:46:02 +0530

    Added both .java and .class files as the commit command when executed, commits all the files that are in staging area.

---
+++ Git.java
@@ -0,0 +1,10 @@
+public class Git{
+    public void branch(){
+        System.out.println("Working with branching concept using new branch called Shadow");
+    }
+
+    public static void main(String[] args) {
+        Git g = new Git();
+        g.branch();
+    }
+}
