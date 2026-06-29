ebf9839b19904d37243f819de4efe49b9855554c
Siddhardha Ram <siddhardharamannem@gmail.com>
2026-06-29 12:45:17 +0530

    Update Git.java

--- Git.java
+++ Git.java
@@ -2,6 +2,7 @@ public class Git{
     public void branch(){
         System.out.println("Working with branching concept using new branch called Shadow");
         System.out.println("Extra statement to know about fetch, merge and pull works");
+        System.out.println("Working with pull request");
     }
 
     public static void main(String[] args) {
